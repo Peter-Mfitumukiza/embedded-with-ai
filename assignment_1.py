aaf9ba1995fea12135a900f1409b3ed09724e75d
Mfitumukiza Peter <61537587+Peter-Mfitumukiza@users.noreply.github.com>
2023-05-09 10:58:11 +0200

    ft: updated assignment_1

--- assignment_1.py
+++ assignment_1.py
@@ -2,7 +2,7 @@
 import cv2
 
 # read the input image
-img = cv2.imread('lena_resized.jpg')
+img = cv2.imread('lena.jpg')
 
 # convert to grayscale of each frames
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -21,8 +21,8 @@ if len(faces) > 0:
         cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 255), 2)
         face = img[y:y + h, x:x + w]
         cv2.imshow(f"Cropped Face {i}", face)
-        cv2.imwrite(f'face{i}.jpg', face)
-        print(f"face{i}.jpg is saved")
+        cv2.imwrite(f'croped_face_{i}.jpg', face)
+        print(f"croped_face_{i}.jpg is saved")
 
 # display the image with detected faces
 cv2.imshow("image", img)
